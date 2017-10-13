b88c70ee4f7a370ad21ae5e35c0cb49ddbdb6799
zeqia <zeqia@130.215.124.99>
2017-10-13 12:00:49 -0400

    add solutionspace

---
+++ src/PuzzlePlane/models/SolutionSpace.java
@@ -0,0 +1,24 @@
+package PuzzlePlane.models;
+
+public class SolutionSpace {
+	ShapeSet shapeSet;
+	PlacedShape[] placedShape;
+	Puzzle puzzle;
+	boolean success;
+	
+	public SolutionSpace( ) {
+		
+	}
+	
+	public void selectShape( ) {
+		
+	}
+	
+	public void addShape() {
+		
+	}
+	
+	public void removeShape() {
+		
+	}
+}
