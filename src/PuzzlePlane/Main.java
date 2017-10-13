63ba0086b08bdf0e45d636a55dd7a9d221cd5f53
zli14 <zli14@DESKTOP-M23A0DR>
2017-10-13 16:48:42 -0400

    Add controller

--- src/PuzzlePlane/Main.java
+++ src/PuzzlePlane/Main.java
@@ -3,9 +3,9 @@ package PuzzlePlane;
 import java.awt.event.*;
 
 import javax.swing.*;
-//TODO import PuzzlePlane.models.*;
+import PuzzlePlane.models.*;
 import PuzzlePlane.views.*;
-//TODO import PuzzlePlane.controllers.*;
+import PuzzlePlane.controllers.*;
 
 public class Main {
 	public static void main(String[] args) {
@@ -15,7 +15,7 @@ public class Main {
 		g.addWindowListener (new WindowAdapter() {
 
 			public void windowClosing(WindowEvent e) {
-				new ExitApplicationController(app).process();
+				new ExitApplicationController(g).process();
 			}
 
 		});
