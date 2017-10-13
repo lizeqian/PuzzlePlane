b2f25428f2ad8421804d5e1779995782b2300567
zeqia <zeqia@130.215.124.99>
2017-10-13 11:27:19 -0400

    Add Board and Palette

---
+++ src/PuzzlePlane/models/Board.java
@@ -0,0 +1,38 @@
+package PuzzlePlane.models;
+
+public class Board {
+	Palette pltt;
+	SolutionSpace sltsp;
+	
+	public Board() {
+	}
+	
+	public void selectShapeSet( ) {
+		
+	}
+	
+	public void selectPuzzle( ) {
+		
+	}
+	
+	public void placeShape( ) {
+		
+	}
+	
+	public void returnShape( ) {
+		
+	}
+	
+	public void reset ( ) {
+		
+	}
+	
+	public void undo ( ) {
+		
+	}
+	
+	public void redo ( ) {
+		
+	}
+	
+}
---
+++ src/PuzzlePlane/models/Palette.java
@@ -0,0 +1,24 @@
+package PuzzlePlane.models;
+
+import java.util.*;
+
+public class Palette {
+	ShapeSet shpst;
+	PlacedShape[] plshp;
+	
+	public Palette( ) {
+		
+	}
+	
+	public void addPlacedShape( ) {
+		
+	}
+	
+	public void addShape() {
+		
+	}
+	
+	public void removeShape() {
+		
+	}
+}
