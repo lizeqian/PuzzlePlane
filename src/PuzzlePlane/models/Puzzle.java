094d14fbe4e9194949f426be0730c913c18a8527
zeqia <lizeqian@gmail.com>
2017-11-03 17:13:35 -0400

    Add basic classes for solution check

--- src/PuzzlePlane/models/Puzzle.java
+++ src/PuzzlePlane/models/Puzzle.java
@@ -8,7 +8,6 @@ import PuzzlePlane.utils.ArrayUtils;
 
 public class Puzzle{
 	List<Polygon> puzzleShape;
-//	PlacedShape puzzleShape;
 	PlacedShape lastSolution;
 	boolean isSolved;
 	
@@ -27,35 +26,4 @@ public class Puzzle{
 		return this.puzzleShape;
 	}
 	
-
-	
-/*	public void setPuzzleShape(PlacedShape puzzleShape) {
-		this.puzzleShape = new ArrayList<PlacedShape>();
-		//TODO: set puzzleShape, avoid shallow copy.
-	}
-	
-	public boolean isSolved(List<PlacedShape> shapes) {
-		int errorNum = 0;
-		int lx = ArrayUtils.min(puzzleShape.originalPolygon.xpoints, puzzleShape.originalPolygon.npoints);
-		int hx = ArrayUtils.max(puzzleShape.originalPolygon.xpoints, puzzleShape.originalPolygon.npoints);
-		int ly = ArrayUtils.min(puzzleShape.originalPolygon.ypoints, puzzleShape.originalPolygon.npoints);
-		int hy = ArrayUtils.max(puzzleShape.originalPolygon.ypoints, puzzleShape.originalPolygon.npoints);
-		
-		for(int i = lx; i <= hx; i++) {
-			for(int j = ly; j <= hy; j++) {
-				boolean found = false;
-				if(!puzzleShape.originalPolygon.contains(i, j)) continue;
-				for(PlacedShape shape : shapes) {
-					Polygon polygon = shape.getChangedPolygon();
-					if(polygon.contains(i, j)) {
-						found = true;
-						break;
-					}
-				}
-				if(!found) errorNum++;
-			}
-		}
-		System.out.println(errorNum);
-		return errorNum < 1000;
-	}*/
 }
