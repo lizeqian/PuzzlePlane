46c0de6e13073ae5f2eeff73b39a9bbd4c9bea8e
zeqia <lizeqian@gmail.com>
2017-11-08 16:51:39 -0500

    Complete TestShapeSet

--- test/plane/models/TestShapeSet.java
+++ test/plane/models/TestShapeSet.java
@@ -3,5 +3,11 @@ package plane.models;
 import junit.framework.TestCase;
 
 public class TestShapeSet extends TestCase {
-
+	public void testGetName() {
+		ShapeSet s = new ShapeSet("testShapeSet");
+		String setName = "setName";
+		s.setName(setName);
+		assertEquals(setName, s.getName());
+		
+	}
 }
