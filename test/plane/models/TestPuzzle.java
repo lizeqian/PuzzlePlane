fb4917ea41b4523b1af8de53fc0e6df43eb892e7
zeqia <lizeqian@gmail.com>
2017-11-08 16:25:24 -0500

    Complete TestPuzzle

--- test/plane/models/TestPuzzle.java
+++ test/plane/models/TestPuzzle.java
@@ -1,7 +1,50 @@
 package plane.models;
 
+import java.awt.Color;
+import java.awt.Polygon;
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 public class TestPuzzle extends TestCase {
-
+	public void testGetLastSolution() {
+		Puzzle p = new Puzzle();
+		ArrayList<PlacedShape> shapes = new ArrayList<PlacedShape>();
+		Polygon polygon = new Polygon();
+		polygon.addPoint(1, 1);
+		polygon.addPoint(2, 2);
+		polygon.addPoint(3, 3);
+		polygon.addPoint(1, 1);
+		PlacedShape shape = new PlacedShape(Color.BLACK);
+		shape.setOriginalPolygon(polygon);
+		shapes.add(shape);
+		p.setLastSolution(shapes);
+		assertEquals(shapes, p.getLastSolution());
+		
+	}
+	
+	public void testSetShape() {
+		ArrayList<PlacedShape> shapesBefore = new ArrayList<PlacedShape>();
+		ArrayList<PlacedShape> shapesAfter = new ArrayList<PlacedShape>();
+		Polygon polygon0 = new Polygon();
+		Polygon polygon1 = new Polygon();
+		polygon0.addPoint(1, 1);
+		polygon0.addPoint(2, 2);
+		polygon0.addPoint(3, 3);
+		polygon0.addPoint(1, 1);
+		polygon1.addPoint(4, 4);
+		polygon1.addPoint(2, 2);
+		polygon1.addPoint(3, 3);
+		polygon1.addPoint(4, 4);
+		PlacedShape shape0 = new PlacedShape(Color.BLACK);
+		PlacedShape shape1 = new PlacedShape(Color.BLUE);
+		shape0.setOriginalPolygon(polygon0);
+		shape1.setOriginalPolygon(polygon1);
+		shapesBefore.add(shape0);
+		shapesAfter.add(shape1);
+		Puzzle p = new Puzzle(shapesBefore);
+		p.setShape(shapesAfter);
+		assertEquals(shapesAfter, p.getPuzzleShape());
+		
+	}
 }
