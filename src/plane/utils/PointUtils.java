12b64610a91d1d728da8a887dc975e08c0cb8b48
uxroc <eterxroc@hotmail.com>
2017-11-09 13:23:53 -0500

    threshold

--- src/plane/utils/PointUtils.java
+++ src/plane/utils/PointUtils.java
@@ -3,7 +3,7 @@ package plane.utils;
 import java.awt.Point;
 
 public class PointUtils {
-	public final static int THRESHOLD = 5;
+	public final static int THRESHOLD = 30;
 	
 	public static boolean equals(Point pointA, Point pointB) {
 		return (Math.abs(pointA.x-pointB.x)<=THRESHOLD) && (Math.abs(pointA.y-pointB.y)<=THRESHOLD);
