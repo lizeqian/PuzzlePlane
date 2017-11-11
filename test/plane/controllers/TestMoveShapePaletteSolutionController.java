1bda247d71be0f8645a013aebe733bf875e5927b
zeqia <lizeqian@gmail.com>
2017-11-11 16:09:24 -0500

    Change puzzlesolvngview initial width and height

--- test/plane/controllers/TestMoveShapePaletteSolutionController.java
+++ test/plane/controllers/TestMoveShapePaletteSolutionController.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 import plane.models.Board;
 import plane.views.Application;
 
-public class TestMoveShapePaletteSolutionController extends TestCase {
+public class TestMoveShapePaletteSolutionController extends ControllerCase {
 	Board board;
 	Application app;
 
