0ddd2900ade2e9fda1d8b4d74e38caba8d34fe74
uxroc <eterxroc@hotmail.com>
2017-11-17 15:04:31 -0500

    utils javadoc

--- src/plane/utils/ColorUtils.java
+++ src/plane/utils/ColorUtils.java
@@ -2,8 +2,18 @@ package plane.utils;
 
 import java.awt.Color;
 
+/**
+ * Define utility methods to operate java,awt.Color class.
+ * @author cheng zhu 
+ *
+ */
 public class ColorUtils {
 	
+	/**
+	 * Returns a copy of a java.awt.Color
+	 * @param color the color
+	 * @return the copy of the color
+	 */
 	public static Color copy(Color color) {
 		return new Color(color.getRed(), color.getGreen(), color.getBlue());
 	}
